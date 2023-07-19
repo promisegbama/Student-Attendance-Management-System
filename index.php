d84fc11b6fce26e3bac272af58acf052d60236ad
promisegbama <137454837+promisegbama@users.noreply.github.com>
2023-07-19 19:52:48 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Student Attendance Management System</title>
+
+  <!-- Bootstrap Core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+ <!-- Custom Fonts -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+  <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+
+  <!-- Custom CSS -->
+  <link href="css/stylish-portfolio.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+  <!-- Header -->
+  <header class="masthead d-flex">
+    <div class="container text-center my-auto">
+       <img src="img/duc_logo.png">
+      <h1 class="mb-1">Welcome</h1>
+      <h3 class="mb-5">
+        <em>to the Student Attendance Management System</em>
+      </h3>
+      <a href="login.php" class="btn btn-primary btn-xl" href="#about">Get Started</a>
+    </div>
+    <div class="overlay"></div>
+  </header>
+</body>
+</html>
